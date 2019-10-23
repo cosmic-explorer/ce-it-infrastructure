3aa69d730f035150d50eac897978f3d77d34d055
Duncan Brown <duncan.brown@ligo.org>
2019-10-23 13:51:09 -0400

    create an initial admin user

--- dcc/docdb.sh
+++ dcc/docdb.sh
@@ -5,6 +5,14 @@ MYSQL_DOCDBRW_PASSWD=$(cat /run/secrets/mysql_docdbrw_passwd)
 MYSQL_DOCDBRO_PASSWD=$(cat /run/secrets/mysql_docdbro_passwd)
 
 mysql -u root << EOF
+USE dcc_docdb;
+INSERT INTO `Author` VALUES (1,'Duncan','A','Brown',50,1,'2019-01-30','dabrown','Duncan A. Brown');
+INSERT INTO `RemoteUser` VALUES (1,'dabrown@syr.edu',1,'dabrown@syr.edu');
+INSERT INTO `EmailUser` VALUES (1,'dabrown','','Duncan Brown','dabrown@syr.edu',0,'2019-01-30',1,1,1,1);
+INSERT INTO `UsersGroup` VALUES (1,1,45,'2019-01-30');
+EOF
+
+mysql -u root << EOF
 SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${MYSQL_ROOT_PASSWD}');
 SET PASSWORD FOR 'root'@'127.0.0.1' = PASSWORD('${MYSQL_ROOT_PASSWD}');
 SET PASSWORD FOR 'docdbrw'@'localhost' = PASSWORD('${MYSQL_DOCDBRW_PASSWD}');
