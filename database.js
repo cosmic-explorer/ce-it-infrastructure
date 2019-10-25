90accfabbfb4b28db3b55eb6812eb03bc0a1a4ef
Duncan Brown <dabrown@syr.edu>
2019-10-25 09:13:11 -0700

    initial entry

---
+++ database.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize')
+const epilogue = require('epilogue')
+const path = require('path')
+
+const database = new Sequelize('dcc_docdb', 'docdbrw', 'herecomethebadgers', {
+  dialect: 'mysql',
+  host: 'localhost',
+  operatorsAliases: false
+})
+
+const Docdb = database.import(path.join(__dirname, 'dcc_docdb/Author'))
+
+const initializeDatabase = async (app) => {
+  epilogue.initialize({ app, sequelize: database })
+
+  epilogue.resource({
+    model: Docdb,
+    endpoints: ['/Author', '/Author/:AuthorID']
+  })
+
+  await database.sync()
+}
+
+module.exports = initializeDatabase
