0155fa00d3f14bb6c3d244a302307e16882b4699
Duncan Brown <dabrown@syr.edu>
2019-10-27 10:06:38 +0000

    wait for port 3306 to open

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const { promisify } = require('util')
+const waitPort = require('wait-port');
+
+const databaseport = {
+  host: 'docdb-database',
+  port: 3306,
+}
 
 const initializeDatabase = require('./database')
 
@@ -8,6 +14,15 @@ const app = express()
 app.use(bodyParser.json())
 
 const startServer = async () => {
+  await waitPort(databaseport)
+    .then((open) => {
+      if (open) console.log('MariaDB is now running')
+      else console.log('The port did not open before the timeout...')
+    })
+    .catch((err) => {
+      console.err(`An unknown error occured while waiting for the port: ${err}`)
+    })
+
   await initializeDatabase(app)
 
   const port = process.env.SERVER_PORT || 8443
