a34df4c7680a33f77b7d43877ee4a2b337a70f86
Duncan Brown <duncan.brown@ligo.org>
2019-11-14 15:20:00 -0500

    added env script

---
+++ mail/mailman-env.sh
@@ -0,0 +1,7 @@
+export STORAGE_PATH=/srv/docker/mailman
+export SERVE_FROM_DOMAIN=seaview.phy.syr.edu
+export MAILMAN_ADMIN_USER=dbrown
+export MAILMAN_ADMIN_EMAIL=dabrown@syr.edu
+export POSTFIX_MAILNAME=seaview.phy.syr.edu
+export POSTFIX_MYHOSTNAME=seaview.phy.syr.edu
+export POSTFIX_MYNETWORKS="192.168.1.0/24"
