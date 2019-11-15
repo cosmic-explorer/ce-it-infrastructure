793834b1eb2a7ffe40e773f918360c3ec16267f4
Duncan Brown <dabrown@syr.edu>
2019-11-14 21:53:43 -0500

    use letsencrypt certs

--- dcc/run-dcc.sh
+++ dcc/run-dcc.sh
@@ -26,15 +26,9 @@ docker swarm init --advertise-addr 127.0.0.1
 
 export CERT_DIR=$(mktemp -d)
 sudo chmod 700 ${CERT_DIR}
-sed -e 's///g' /etc/grid-security/igtf-ca-bundle.crt > ${CERT_DIR}/igtf-ca-bundle.crt
-sudo cp -a /etc/grid-security/hostkey.pem ${CERT_DIR}
-sudo cp -a /etc/grid-security/hostcert.pem ${CERT_DIR}
 sudo cp -a /etc/shibboleth/sp-encrypt-cert.pem ${CERT_DIR}
 sudo cp -a /etc/shibboleth/sp-encrypt-key.pem ${CERT_DIR}
 sudo chown ${USER} ${CERT_DIR}/*.pem
-docker secret create https_chain_file ${CERT_DIR}/igtf-ca-bundle.crt
-docker secret create https_cert_file ${CERT_DIR}/hostcert.pem
-docker secret create https_privkey_file ${CERT_DIR}/hostkey.pem
 docker secret create shibboleth_sp_encrypt_cert ${CERT_DIR}/sp-encrypt-cert.pem
 docker secret create shibboleth_sp_encrypt_privkey ${CERT_DIR}/sp-encrypt-key.pem
 echo ${MYSQL_ROOT_PASSWD} | docker secret create mysql_root_passwd -
