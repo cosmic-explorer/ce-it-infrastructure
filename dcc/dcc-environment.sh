a4be192fbdefd2a4c4701527bee60e2e0c3ded9b
Duncan Brown <dabrown@syr.edu>
2019-12-10 17:24:13 -0500

    set real defaults

--- dcc/dcc-environment.sh
+++ dcc/dcc-environment.sh
@@ -1,12 +1,12 @@
 export STORAGE_PATH=/srv/docker/dcc
-export DCC_INSTANCE=seaview.phy.syr.edu
-export DCC_HOSTNAME=seaview.phy.syr.edu
-export DCC_DOMAINNAME=phy.syr.edu
+export DCC_INSTANCE=dcc.cosmicexplorer.org
+export DCC_HOSTNAME=dcc.cosmicexplorer.org
+export DCC_DOMAINNAME=cosmicexplorer.org
 export MYSQL_ROOT_PASSWD=badgers
 export MYSQL_DOCDBRW_PASSWD=mushroommushroom
 export MYSQL_DOCDBRO_PASSWD=badgersbadgersbadgers
 export HYDRA_PASSWD=aghitsasnake
 export SECRETS_SYSTEM=itsabadgersbadgersbadgers
 export DCC_REST_SECRET=oooosnake
-export REST_AUTHORIZED_EPPN=user@nohost.com
+export REST_AUTHORIZED_EPPN=dabrown@syr.edu
 export SHIB_HEADER_SECRET=mushroom
