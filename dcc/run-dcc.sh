4d7af8034574870556117ca177a0171b053a7ff1
Duncan Brown <dabrown@syr.edu>
2019-12-11 12:21:06 -0500

    use txt secrets rather than swarm

--- dcc/run-dcc.sh
+++ dcc/run-dcc.sh
@@ -7,24 +7,31 @@ if [ ! -d ${STORAGE_PATH} ] ; then
   kill -INT $$
 fi
 
-docker swarm leave --force &>/dev/null || true
-docker swarm init --advertise-addr 127.0.0.1
+# FIXME
+# docker swarm leave --force &>/dev/null || true
+# docker swarm init --advertise-addr 127.0.0.1
 
 export CERT_DIR=$(mktemp -d)
 sudo chmod 700 ${CERT_DIR}
-sudo cp -a /etc/shibboleth/sp-encrypt-cert.pem ${CERT_DIR}
-sudo cp -a /etc/shibboleth/sp-encrypt-key.pem ${CERT_DIR}
+sudo cp -a ${APACHE_SHIBD_DIR}/shibboleth/sp-encrypt-cert.pem ${CERT_DIR}
+sudo cp -a ${APACHE_SHIBD_DIR}/shibboleth/sp-encrypt-key.pem ${CERT_DIR}
 sudo chown ${USER} ${CERT_DIR}/*.pem
-docker secret create shibboleth_sp_encrypt_cert ${CERT_DIR}/sp-encrypt-cert.pem
-docker secret create shibboleth_sp_encrypt_privkey ${CERT_DIR}/sp-encrypt-key.pem
-echo ${MYSQL_ROOT_PASSWD} | docker secret create mysql_root_passwd -
-echo ${MYSQL_ROOT_PASSWD}  | docker secret create mariadb_root_password -
-echo ${MYSQL_DOCDBRW_PASSWD} | docker secret create mysql_docdbrw_passwd -
-echo ${MYSQL_DOCDBRO_PASSWD} | docker secret create mysql_docdbro_passwd -
-echo ${SHIB_HEADER_SECRET} | docker secret create shib_header_secret -
-echo ${REST_AUTHORIZED_EPPN} | docker secret create rest_authorized_eppn -
+
+# FIXME
+# docker secret create shibboleth_sp_encrypt_cert ${CERT_DIR}/sp-encrypt-cert.pem
+# docker secret create shibboleth_sp_encrypt_privkey ${CERT_DIR}/sp-encrypt-key.pem
+# echo ${MYSQL_ROOT_PASSWD} | docker secret create mysql_root_passwd -
+# echo ${MYSQL_ROOT_PASSWD}  | docker secret create mariadb_root_password -
+# echo ${MYSQL_DOCDBRW_PASSWD} | docker secret create mysql_docdbrw_passwd -
+# echo ${MYSQL_DOCDBRO_PASSWD} | docker secret create mysql_docdbro_passwd -
+# echo ${SHIB_HEADER_SECRET} | docker secret create shib_header_secret -
+# echo ${REST_AUTHORIZED_EPPN} | docker secret create rest_authorized_eppn -
+cat sp-encrypt-cert.pem > ./shibboleth_sp_encrypt_cert.txt
+cat sp-encrypt-key.pem > ./shibboleth_sp_encrypt_privkey.txt
+
 rm -rf ${CERT_DIR}
 
-docker stack deploy --compose-file dcc.yml dcc
+# FIXME
+# docker stack deploy --compose-file dcc.yml dcc
 
 trap - ERR
