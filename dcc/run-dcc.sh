6d0bb3c4dc2ba7daa8ad8dcbfd8ee68f7ecc6afb
Duncan Brown <dabrown@syr.edu>
2019-12-19 16:00:50 -0500

    use swarm

--- dcc/run-dcc.sh
+++ dcc/run-dcc.sh
@@ -7,9 +7,8 @@ if [ ! -d ${STORAGE_PATH} ] ; then
   kill -INT $$
 fi
 
-# FIXME
-# docker swarm leave --force &>/dev/null || true
-# docker swarm init --advertise-addr 127.0.0.1
+docker swarm leave --force &>/dev/null || true
+docker swarm init --advertise-addr 127.0.0.1
 
 export CERT_DIR=$(mktemp -d)
 sudo chmod 700 ${CERT_DIR}
@@ -17,21 +16,17 @@ sudo cp -a ${APACHE_SHIBD_DIR}/shibboleth/sp-encrypt-cert.pem ${CERT_DIR}
 sudo cp -a ${APACHE_SHIBD_DIR}/shibboleth/sp-encrypt-key.pem ${CERT_DIR}
 sudo chown ${USER} ${CERT_DIR}/*.pem
 
-# FIXME
-# docker secret create shibboleth_sp_encrypt_cert ${CERT_DIR}/sp-encrypt-cert.pem
-# docker secret create shibboleth_sp_encrypt_privkey ${CERT_DIR}/sp-encrypt-key.pem
-# echo ${MYSQL_ROOT_PASSWD} | docker secret create mysql_root_passwd -
-# echo ${MYSQL_ROOT_PASSWD}  | docker secret create mariadb_root_password -
-# echo ${MYSQL_DOCDBRW_PASSWD} | docker secret create mysql_docdbrw_passwd -
-# echo ${MYSQL_DOCDBRO_PASSWD} | docker secret create mysql_docdbro_passwd -
-# echo ${SHIB_HEADER_SECRET} | docker secret create shib_header_secret -
-# echo ${REST_AUTHORIZED_EPPN} | docker secret create rest_authorized_eppn -
-cat ${CERT_DIR}/sp-encrypt-cert.pem > ./shibboleth_sp_encrypt_cert.txt
-cat ${CERT_DIR}/sp-encrypt-key.pem > ./shibboleth_sp_encrypt_privkey.txt
+docker secret create shibboleth_sp_encrypt_cert ${CERT_DIR}/sp-encrypt-cert.pem
+docker secret create shibboleth_sp_encrypt_privkey ${CERT_DIR}/sp-encrypt-key.pem
+echo ${MYSQL_ROOT_PASSWD} | docker secret create mysql_root_passwd -
+echo ${MYSQL_ROOT_PASSWD}  | docker secret create mariadb_root_password -
+echo ${MYSQL_DOCDBRW_PASSWD} | docker secret create mysql_docdbrw_passwd -
+echo ${MYSQL_DOCDBRO_PASSWD} | docker secret create mysql_docdbro_passwd -
+echo ${SHIB_HEADER_SECRET} | docker secret create shib_header_secret -
+echo ${REST_AUTHORIZED_EPPN} | docker secret create rest_authorized_eppn -
 
 rm -rf ${CERT_DIR}
 
-# FIXME
-# docker stack deploy --compose-file dcc.yml dcc
+docker stack deploy --compose-file dcc.yml dcc
 
 trap - ERR
