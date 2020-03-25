32a131a73b3c23fff1803b92ccbcee42692a856b
Duncan Brown <dabrown@syr.edu>
2020-03-25 09:39:07 -0400

    update for compose

--- mail/mailman-env.sh
+++ mail/mailman-env.sh
@@ -1,37 +1,33 @@
 export STORAGE_PATH=/srv/docker/mailman
 export APACHE_SHIBD_DIR=/root/ce-mail/apache-shibd
 
+export HYPERKITTY_API_KEY="badgers"
+export POSTGRESS_PASSWORD="badgers"
+export MAILMAN_REST_PASSWORD="badgers"
+export MAILMAN_WEB_SECRET_KEY="badgers"
+
 export SERVE_FROM_DOMAIN=mail.cosmicexplorer.org
 export MAILMAN_ADMIN_USER=dbrown
 export MAILMAN_ADMIN_EMAIL=dabrown@syr.edu
+
 export POSTFIX_MAILNAME=mail.cosmicexplorer.org
 export POSTFIX_MYHOSTNAME=mail.cosmicexplorer.org
 export POSTFIX_MYNETWORKS="192.168.102.0/24"
 export POSTFIX_RELAYHOST="smtp-host.syr.edu"
 
+export SECRET_KEY="badgers"
+
+# FIXME: need to check these
+#export DJANGO_ALLOWED_HOSTS=""
+#export MAILMAN_HOST_IP=""
+#export SMTP_HOST="smtp-host.syr.edu"
+
+# temporary storage of secrets until we have macvlan
+echo "${HYPERKITTY_API_KEY}" > ./hyperkitty_api_key.txt
+echo "${POSTGRESS_PASSWORD}" > ./postgres_password.txt
+echo "postgres://mailman:${POSTGRESS_PASSWORD}@database/mailmandb" > ./mailman_database_url.txt
+echo "${MAILMAN_REST_PASSWORD}" > ./mailman_rest_password.txt
+echo "${MAILMAN_WEB_SECRET_KEY}" > ./mailman_web_secret_key.txt
+
 # temporary use of compose until we have macvlan
 export COMPOSE_IGNORE_ORPHANS=1
-
-export HYPERKITTY_API_KEY
-export MAILMAN_DATABASE_URL
-export MAILMAN_REST_PASSWORD
-export MAILMAN_WEB_SECRET_KEY
-
-export SECRET_KEY
-export SERVE_FROM_DOMAIN
-export DJANGO_ALLOWED_HOSTS
-export MAILMAN_REST_URL="http://mailman-core:8001"
-export MAILMAN_REST_USER
-export MAILMAN_REST_PASSWORD
-export HYPERKITTY_API_KEY
-export MAILMAN_HOST_IP
-
-export SERVE_FROM_DOMAIN
-export SMTP_HOST
-export SMTP_PORT
-export SMTP_HOST_USER
-export SMTP_HOST_PASSWORD
-export SMTP_USE_TLS="False"
-export DJANGO_LOG_URL
-
-export POSTORIUS_TEMPLATE_BASE_URL="http://mailman-web:8000"
