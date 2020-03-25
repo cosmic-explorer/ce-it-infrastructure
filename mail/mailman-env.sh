8bc52bda7856f9a4682d7e2442ed97e8a2c058f6
Duncan Brown <dabrown@syr.edu>
2020-03-25 08:53:45 -0400

    list of env vars from scanning setup files

--- mail/mailman-env.sh
+++ mail/mailman-env.sh
@@ -1,7 +1,36 @@
 export STORAGE_PATH=/srv/docker/mailman
-export SERVE_FROM_DOMAIN=seaview.phy.syr.edu
+export APACHE_SHIBD_DIR=/root/ce-mail/apache-shibd
+
+export SERVE_FROM_DOMAIN=mail.cosmicexplorer.org
 export MAILMAN_ADMIN_USER=dbrown
 export MAILMAN_ADMIN_EMAIL=dabrown@syr.edu
-export POSTFIX_MAILNAME=seaview.phy.syr.edu
-export POSTFIX_MYHOSTNAME=seaview.phy.syr.edu
-export POSTFIX_MYNETWORKS="192.168.1.0/24"
+export POSTFIX_MAILNAME=mail.cosmicexplorer.org
+export POSTFIX_MYHOSTNAME=mail.cosmicexplorer.org
+export POSTFIX_MYNETWORKS="192.168.102.0/24"
+
+# temporary use of compose until we have macvlan
+export COMPOSE_IGNORE_ORPHANS=1
+
+export HYPERKITTY_API_KEY
+export MAILMAN_DATABASE_URL
+export MAILMAN_REST_PASSWORD
+export MAILMAN_WEB_SECRET_KEY
+
+export SECRET_KEY
+export SERVE_FROM_DOMAIN
+export DJANGO_ALLOWED_HOSTS
+export MAILMAN_REST_URL="http://mailman-core:8001"
+export MAILMAN_REST_USER
+export MAILMAN_REST_PASSWORD
+export HYPERKITTY_API_KEY
+export MAILMAN_HOST_IP
+
+export SERVE_FROM_DOMAIN
+export SMTP_HOST
+export SMTP_PORT
+export SMTP_HOST_USER
+export SMTP_HOST_PASSWORD
+export SMTP_USE_TLS="False"
+export DJANGO_LOG_URL
+
+export POSTORIUS_TEMPLATE_BASE_URL="http://mailman-web:8000"
