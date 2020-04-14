bc01ba3b7ebe6a318a1d29aa365189fae8dbcbb6
Duncan Brown <dabrown@syr.edu>
2020-04-14 16:52:04 -0400

    Create run-mailman.sh

---
+++ mail/run-mailman.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -v
+
+set -e
+
+. mailman-env.sh
+
+docker-compose --file=letsencrypt.yml up --detach
+docker-compose --file=mailman-database.yml up --detach
+docker-compose --file=mailman-postfix.yml up --detach
+docker-compose --file=mailman-web.yml up --detach
+docker-compose --file=mailman-apache.yml up --detach
+
+exit 0
