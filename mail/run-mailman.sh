7a56f279dc28bf3529027d92a393d4e5dc9d8814
Duncan Brown <dabrown@syr.edu>
2020-04-14 16:56:29 -0400

    add core

--- mail/run-mailman.sh
+++ mail/run-mailman.sh
@@ -8,6 +8,7 @@ docker-compose --file=letsencrypt.yml up --detach
 docker-compose --file=mailman-database.yml up --detach
 docker-compose --file=mailman-postfix.yml up --detach
 docker-compose --file=mailman-web.yml up --detach
+docker-compose --file=mailman-core.yml up --detach
 docker-compose --file=mailman-apache.yml up --detach
 
 exit 0
