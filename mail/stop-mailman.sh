98a9d08c10b049354c7dd5fc31f2fe3d2492a419
Duncan Brown <dabrown@syr.edu>
2020-04-14 16:56:09 -0400

    add core and reverse order

--- mail/stop-mailman.sh
+++ mail/stop-mailman.sh
@@ -4,10 +4,11 @@ set -e
 
 . mailman-env.sh
 
-docker-compose --file=letsencrypt.yml down
-docker-compose --file=mailman-database.yml down
-docker-compose --file=mailman-postfix.yml down
-docker-compose --file=mailman-web.yml down
 docker-compose --file=mailman-apache.yml down
+docker-compose --file=mailman-core.yml down
+docker-compose --file=mailman-web.yml down
+docker-compose --file=mailman-postfix.yml down
+docker-compose --file=mailman-database.yml down
+docker-compose --file=letsencrypt.yml down
 
 exit 0
