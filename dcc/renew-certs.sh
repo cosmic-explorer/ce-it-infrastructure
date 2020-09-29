7895a1d4dc419abba914de3a154492a9d84de24b
Duncan Brown <dabrown@syr.edu>
2020-09-29 10:06:27 -0400

    symlink certs once

--- dcc/renew-certs.sh
+++ dcc/renew-certs.sh
@@ -2,13 +2,6 @@
 
 set -x
 
-DCC_INSTANCE=$(awk '/DCC_INSTANCE/ {print $3}' /etc/httpd/conf/httpd.conf)
-cp -v /etc/letsencrypt/live/${DCC_INSTANCE}/cert.pem /etc/httpd/x509-certs/${DCC_INSTANCE}.pem
-cp -v /etc/letsencrypt/live/${DCC_INSTANCE}/privkey.pem /etc/httpd/x509-certs/${DCC_INSTANCE}.key
-cp -v /etc/letsencrypt/live/${DCC_INSTANCE}/fullchain.pem /etc/httpd/x509-certs/${DCC_INSTANCE}.cert
-chmod 0444 /etc/httpd/x509-certs/${DCC_INSTANCE}.pem /etc/httpd/x509-certs/${DCC_INSTANCE}.cert
-chmod 0400 /etc/httpd/x509-certs/${DCC_INSTANCE}.key
-
 httpd -k graceful
 
 exit 0
